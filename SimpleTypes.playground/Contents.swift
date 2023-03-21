946d8b299de5b37f7e9a3fb178bd98e4e6e242f6
JazepsIvulis <jazeps.ivulis@gmail.com>
2023-03-21 21:42:32 +0200

    Add SimpleTypes playground

---
+++ SimpleTypes.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+// Variables
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+
+// Strings and integers
+var jedi = "Hello there"
+var age = 29
+var population = 2_000_000
+
+// Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let album: String = "Antares"
+let year: Int = 1994
+let height: Double = 1.82
+let taylorRocks: Bool = false
